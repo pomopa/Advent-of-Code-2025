14686d1e2c36e472798d309045836e6ebd457b99
Pol Monne <pol.monne@students.salle.url.edu>
2025-12-05 09:34:05 -0600

    Basic Structure for exercise 3 implemented.

---
+++ src/days/day03/Lobby.java
@@ -0,0 +1,17 @@
+package days.day03;
+
+import core.Solver;
+
+import java.util.List;
+
+public class Lobby implements Solver {
+    @Override
+    public long solveSilver(List<String> input) {
+        return 0;
+    }
+
+    @Override
+    public long solveGold(List<String> input) {
+        return 0;
+    }
+}
