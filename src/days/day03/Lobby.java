d328dbeca2613e3771656dd7fb5dbbf2ad8c8f41
Pol Monne <pol.monne@students.salle.url.edu>
2025-12-05 10:12:31 -0600

    Solution for advent of code day 3 silver solved.

--- src/days/day03/Lobby.java
+++ src/days/day03/Lobby.java
@@ -7,7 +7,36 @@ import java.util.List;
 public class Lobby implements Solver {
     @Override
     public long solveSilver(List<String> input) {
-        return 0;
+        long joltage = 0;
+
+        for (String line : input) {
+            int n = line.length();
+
+            int[] maxRight = new int[n];
+            int currMax = 0;
+
+            for (int i = n - 1; i >= 0; i--) {
+                int d = line.charAt(i) - '0';
+                currMax = Math.max(currMax, d);
+                maxRight[i] = currMax;
+            }
+
+            int best = 0;
+
+            for (int i = 0; i < n - 1; i++) {
+                int left = line.charAt(i) - '0';
+                int right = maxRight[i + 1];
+
+                int candidate = left * 10 + right;
+                if (candidate > best) best = candidate;
+
+                if (best == 99) break;
+            }
+
+            joltage += best;
+        }
+
+        return joltage;
     }
 
     @Override
