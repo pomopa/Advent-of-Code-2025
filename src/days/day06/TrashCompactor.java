95414153cf2b4f9e45d8915acb80ef859550def2
Pol Monne <pol.monne@students.salle.url.edu>
2025-12-06 00:09:04 -0600

    Solution for day 6 silver star

--- src/days/day06/TrashCompactor.java
+++ src/days/day06/TrashCompactor.java
@@ -2,12 +2,38 @@ package days.day06;
 
 import core.Solver;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TrashCompactor implements Solver {
     @Override
     public long solveSilver(List<String> input) {
-        return 0;
+        long result = 0;
+        List<String[]> values = new ArrayList<>();
+        for (String line : input) {
+            line = line.trim();
+            String[] parts = line.split("\\s+");
+            values.add(parts);
+        }
+        int columns = values.get(0).length;
+        int lines = values.size();
+
+        for (int i = 0; i < columns; i++) {
+            String operator = values.get(lines - 1)[i];
+            long temp = Long.parseLong(values.get(0)[i]);
+            if (operator.equals("+")) {
+                for (int j = 1; j < lines - 1; j++) {
+                    temp += Long.parseLong(values.get(j)[i]);
+                }
+            } else {
+                for (int j = 1; j < lines - 1; j++) {
+                    temp *= Long.parseLong(values.get(j)[i]);
+                }
+            }
+            result += temp;
+        }
+
+        return result;
     }
 
     @Override
