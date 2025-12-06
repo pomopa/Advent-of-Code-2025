0a867a4a84526a05bb45ef9b54fc06b6b64cc23e
Pol Monne <pol.monne@students.salle.url.edu>
2025-12-05 21:32:52 -0600

    Basic structure to solve Day 5 silver and gold star

---
+++ src/days/day05/Cafeteria.java
@@ -0,0 +1,17 @@
+package days.day05;
+
+import core.Solver;
+
+import java.util.List;
+
+public class Cafeteria implements Solver {
+    @Override
+    public long solveSilver(List<String> input) {
+        return 0;
+    }
+
+    @Override
+    public long solveGold(List<String> input) {
+        return 0;
+    }
+}
