114b0e89bc39604a4a28373938802c6d3baa3796
Pol Monne <pol.monne@students.salle.url.edu>
2025-12-08 23:22:09 -0600

    Solution for day 09 implemented. Brute force approach probably not optimal.

--- src/days/day09/MovieTheater.java
+++ src/days/day09/MovieTheater.java
@@ -5,9 +5,40 @@ import core.Solver;
 import java.util.List;
 
 public class MovieTheater implements Solver {
+    private record Point(long x, long y) {}
+    private Point[] parsePoints(List<String> input) {
+        int n = input.size();
+        Point[] pts = new Point[n];
+
+        for (int i = 0; i < n; i++) {
+            String[] p = input.get(i).split(",");
+            pts[i] = new Point(
+                    Long.parseLong(p[0]),
+                    Long.parseLong(p[1])
+            );
+        }
+        return pts;
+    }
+
+    private long calculateArea(Point a, Point b) {
+        return (Math.abs(a.x - b.x) + 1) * (Math.abs(a.y - b.y) + 1);
+    }
+
     @Override
     public long solveSilver(List<String> input) {
-        return 0;
+        Point[] pts = parsePoints(input);
+        long area = 0;
+
+        for (int i = 0; i < pts.length; i++) {
+            for (int j = i + 1; j < pts.length; j++) {
+                long temp = calculateArea(pts[i], pts[j]);
+                if (temp > area) {
+                    area = temp;
+                }
+            }
+        }
+
+        return area;
     }
 
     @Override
