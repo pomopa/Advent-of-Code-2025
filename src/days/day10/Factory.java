399362da7a9ad9890d44f4fd0ae60986a9c2887e
Pol Monne <pol.monne@students.salle.url.edu>
2025-12-10 19:43:30 -0600

    Initial structure implementation for day 10 solutions

---
+++ src/days/day10/Factory.java
@@ -0,0 +1,17 @@
+package days.day10;
+
+import core.Solver;
+
+import java.util.List;
+
+public class Factory implements Solver {
+    @Override
+    public long solveSilver(List<String> input) {
+        return 0;
+    }
+
+    @Override
+    public long solveGold(List<String> input) {
+        return 0;
+    }
+}
